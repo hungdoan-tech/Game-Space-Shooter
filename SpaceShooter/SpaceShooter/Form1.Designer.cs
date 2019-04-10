50f42f2523235e5c1c92405dc8de736606ba483e
hungdoan7 <40148248+hungdoan7@users.noreply.github.com>
2019-04-10 12:43:36 +0700

    BackGround

--- SpaceShooter/SpaceShooter/Form1.Designer.cs
+++ SpaceShooter/SpaceShooter/Form1.Designer.cs
@@ -29,12 +29,81 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.StarTimer = new System.Windows.Forms.Timer(this.components);
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.Exit_Button = new System.Windows.Forms.Button();
+            this.Main_PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Main_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StarTimer
+            // 
+            this.StarTimer.Enabled = true;
+            this.StarTimer.Interval = 10;
+            this.StarTimer.Tick += new System.EventHandler(this.StarTimer_Tick);
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Start_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Start_Button.Location = new System.Drawing.Point(200, 229);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(93, 44);
+            this.Start_Button.TabIndex = 1;
+            this.Start_Button.Text = "Start";
+            this.Start_Button.UseVisualStyleBackColor = false;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.BackColor = System.Drawing.Color.Red;
+            this.Exit_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Exit_Button.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit_Button.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Exit_Button.Location = new System.Drawing.Point(200, 290);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.Size = new System.Drawing.Size(93, 40);
+            this.Exit_Button.TabIndex = 2;
+            this.Exit_Button.Text = "Exit";
+            this.Exit_Button.UseVisualStyleBackColor = false;
+            this.Exit_Button.Click += new System.EventHandler(this.Exit_Button_Click);
+            // 
+            // Main_PictureBox
+            // 
+            this.Main_PictureBox.BackColor = System.Drawing.Color.White;
+            this.Main_PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Main_PictureBox.Location = new System.Drawing.Point(1, 1);
+            this.Main_PictureBox.MaximumSize = new System.Drawing.Size(500, 600);
+            this.Main_PictureBox.Name = "Main_PictureBox";
+            this.Main_PictureBox.Size = new System.Drawing.Size(500, 600);
+            this.Main_PictureBox.TabIndex = 3;
+            this.Main_PictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(503, 599);
+            this.Controls.Add(this.Exit_Button);
+            this.Controls.Add(this.Start_Button);
+            this.Controls.Add(this.Main_PictureBox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximumSize = new System.Drawing.Size(519, 638);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Main_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer StarTimer;
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.Button Exit_Button;
+        private System.Windows.Forms.PictureBox Main_PictureBox;
     }
 }
 
--- SpaceShooter/SpaceShooter/Form1.cs
+++ SpaceShooter/SpaceShooter/Form1.cs
@@ -12,9 +12,88 @@ namespace SpaceShooter
 {
     public partial class Form1 : Form
     {
+        Graphics gp;
+        Bitmap bitmap;
+        Random rd;
+        star[] ListStar= new star[13];  
         public Form1()
         {
             InitializeComponent();
+            bitmap = new Bitmap(Main_PictureBox.Width, Main_PictureBox.Height);
+            gp = Graphics.FromImage(bitmap);
+            gp.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            rd = new Random();
+            for (int i = 0; i < ListStar.Length; i++)
+            {
+                ListStar[i] = new star();
+                ListStar[i].x = rd.Next(0, 500);
+                ListStar[i].y = rd.Next(100, 600);
+                ListStar[i].StarType = rd.Next(0, 2);
+            }  
+        }
+
+        private void StarTimer_Tick(object sender, EventArgs e)
+        {
+            rd = new Random();          
+            for (int i = 0; i < ListStar.Length; i++)
+            {
+                if (ListStar[i].StarType == 1)
+                {
+                    ListStar[i].y += 3;
+                }
+                else
+                {
+                    ListStar[i].y += 5;
+                }
+            }
+            for (int i = 0; i < ListStar.Length; i++)
+            {
+                if (ListStar[i].y >= Main_PictureBox.Height)
+                {
+                    ListStar[i].x = rd.Next(0, 500);
+                    ListStar[i].y = 0;
+                    ListStar[i].StarType = rd.Next(0, 2);
+                }
+            }
+            Draw();
+        }
+        public void Draw()
+        {
+            gp.Clear(Color.White);
+
+            gp.FillRectangle(new SolidBrush(Color.FromArgb(255, (byte)0, (byte)0, (byte)56)), 0, 0, Main_PictureBox.Width, Main_PictureBox.Height);
+          
+            for (int i = 0; i < ListStar.Length; i++)
+            {
+                if (ListStar[i].StarType == 1)
+                {
+                    gp.DrawImage(Properties.Resources.star1, ListStar[i].x, ListStar[i].y);
+                }
+                else
+                {
+                    if (ListStar[i].StarType == 0 || ListStar[i].StarType == 2)
+                        gp.DrawImage(Properties.Resources.star2, ListStar[i].x, ListStar[i].y);
+                }
+            }
+            Main_PictureBox.Image = bitmap;
+        }
+        public class star
+        {
+            public float x;
+            public float y;
+            public int StarType;
+        }
+
+        private void Start_Button_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 ab = new Form2();
+            ab.ShowDialog();   
+        }
+
+        private void Exit_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
